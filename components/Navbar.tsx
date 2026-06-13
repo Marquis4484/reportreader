1d61c680354f3e692e66015ebb78538d5edde859
Marquis Sampson <marquis4484@gmail.com>
2026-06-13 11:42:04 -0400

    Title Change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,8 +20,8 @@ const Navbar = () => {
         <header className="w-full fixed z-50 bg-(--bg-primary)">
             <div className="wrapper navbar-height py-4 flex justify-between items-center">
                 <Link href="/" className="flex gap-0.5 items-center">
-                    <Image src="/assets/logo.png" alt="Bookfied" width={42} height={26} />
-                    <span className="logo-text">ReportReader</span>
+                    <Image src="/assets/ReportReaderTitle.png" alt="ReportReader" width={240} height={240}/>
+                   
                 </Link>
 
                 <nav className="w-fit flex gap-7.5 items-center">
