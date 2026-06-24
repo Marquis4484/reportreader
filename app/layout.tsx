38fdcab940d55401f42da9480e16dd026c5543b2
Marquis Sampson <marquis4484@gmail.com>
2026-06-24 12:49:22 -0400

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { IBM_Plex_Serif, Mona_Sans} from "next/font/google";
-
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import {Toaster} from "@/components/ui/sonner";
